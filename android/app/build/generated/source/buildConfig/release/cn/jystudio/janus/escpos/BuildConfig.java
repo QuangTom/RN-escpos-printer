0f787b7d4172194ffd629ff7949cd89e7b23f9fa
baquang.nydo@gmail.com <>
2021-06-20 22:51:27 +0700

    - Init

---
+++ android/app/build/generated/source/buildConfig/release/cn/jystudio/janus/escpos/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package cn.jystudio.janus.escpos;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "cn.jystudio.janus.escpos";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
